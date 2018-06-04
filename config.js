b9d21a5d4aa050532e652908c00d3d36a0b9315e
Lonny Gomes <code@lonnygomes.com>
2018-06-03 21:20:00 -0400

    Add references to cert paths
    The certs folder is also ignored as it should not be commited to the repo

--- config.js
+++ config.js
@@ -1,3 +1,10 @@
 module.exports = {
-    cronSchedule: '*/10 * * * * *'
+    cronSchedule: '*/10 * * * * *',
+    bucketName: 'lonnygomes-timelapse',
+    bucketPrefix: 'tl-plant',
+    credentials: {
+        keyPath: 'certs/private.key',
+        certPath: 'certs/cert.pem',
+        caPath: 'certs/root-CA.crt'
+    }
 }
