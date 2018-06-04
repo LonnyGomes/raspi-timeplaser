15cb712b83032ca98dbda845f8e5cacd0e68a9a8
Lonny Gomes <code@lonnygomes.com>
2018-06-03 23:10:57 -0400

    Add S3 upload capability
    Uploads captured image to s3 and removes it from disk upon successful upload
    
    This will enable long term time lapses without filling up the space on the local device

--- src/timelapseTask.js
+++ src/timelapseTask.js
@@ -1,5 +1,9 @@
 const State = require('./state');
 const state = new State();
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+const fs = require('fs-extra');
+const path = require('path');
 
 const bootstrap = (config) => {
 
@@ -14,6 +18,18 @@ const bootstrap = (config) => {
         return myCamera;
     }
 
+    const upload = (params) => {
+        return new Promise((resolve, reject) => {
+            s3.upload(params, (err, data) => {
+                if (err) {
+                    return reject(err);
+                }
+
+                return resolve(data);
+            });
+        });
+    }
+
     const task = async () => {
         const curIdx = await state.getCurIdx();
         console.log(`[*] creating time lapse image using index ${curIdx}`);
@@ -21,11 +37,27 @@ const bootstrap = (config) => {
         try {
             // create instance of camera object
             const cam = createCam(curIdx);
+            const outputFile = cam.config.output;
+            const filename = path.basename(outputFile);
 
             // take picture
             await cam.snap();
 
-            console.log('[*] captured photo');
+            console.log(`[*] captured ${filename}`);
+
+            // load photo in as a buffer
+            const photoData = await fs.readFile(outputFile);
+
+            console.log(`[*] uploading ${filename} ..`);
+            await upload({
+                Bucket: config.bucketName,
+                Key: `${config.bucketPrefix}/${filename}`,
+                Body: photoData
+            });
+            console.log(`[*] upload completed for ${filename}`);
+
+            // remove image now that it is in s3
+            await fs.remove(outputFile);
 
             // update index upon successful capture
             state.updateCurIdx();
