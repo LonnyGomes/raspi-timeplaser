830ce2d3a46d0e629d74737a473f5a3e9c4cc553
Lonny Gomes <code@lonnygomes.com>
2018-06-03 22:31:13 -0400

    Add code to capture image from pi camera

--- src/timelapseTask.js
+++ src/timelapseTask.js
@@ -3,13 +3,40 @@ const state = new State();
 
 const bootstrap = (params) => {
 
+    const createCam = (idx) => {
+        const PiCamera = require('pi-camera');
+        const myCamera = new PiCamera({
+            mode: 'photo',
+            output: `${ __dirname }/tl-${idx}.jpg`,
+            nopreview: true,
+        });
+
+        return myCamera;
+    }
+
     const task = async () => {
         const curIdx = await state.getCurIdx();
-        console.log(`creating time lapse image using index ${curIdx}`);
-        state.updateCurIdx();
+        console.log(`[*] creating time lapse image using index ${curIdx}`);
+
+        try {
+            // create instance of camera object
+            const cam = createCam(curIdx);
+
+            // take picture
+            await cam.snap();
+
+            console.log('[*] captured photo');
+
+            // update index upon successful capture
+            state.updateCurIdx();
+        } catch (err) {
+            console.error('Error while capturing photo', err);
+        }
+
     }
 
     return task;
 }
 
 module.exports = bootstrap;
+
